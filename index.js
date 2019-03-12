5b470a61530cc09cd7dfda8bdf328e23fc5994d7
DevFenix <33089393+DevFenix@users.noreply.github.com>
2019-03-12 21:32:11 +0100

    Command Prefix + Arguments structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
-const Discord = require('discord.js')
-const client = new Discord.Client();
+const {Client, RichEmbed} = require('discord.js')
+const client = new Client();
+const config = require('./config.json')
 
 dotenv.load();
 
@@ -9,10 +10,37 @@ client.on('ready', () => {
   });
 
 client.on('message', msg => {
-    if(msg.content === 'ping')
-        msg.reply('pong')
+  if (msg.content.indexOf(config.prefix) !== 0) return;
+ 
+  const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === 'ping')
+    msg.reply('pong')
+
+  if(command === 'marquito'){
+    const embed = new RichEmbed()
+      .setTitle("Marquito")
+      .setColor('0xFF0000')
+      .setDescription('Esto es la descripción')
+    msg.channel.send(embed)
+  }
+
+  if(command === 'myavatar')
+    msg.reply(msg.author.avatarURL)
+
+  if(command === 'avatar'){
+    if (args[0] == null)  return;
+    let localUser = msg.mentions.users.first()
+    msg.channel.send(localUser.avatarURL)
+  }
+
   });
 
-console.log(process.env.TOKEN)
+  client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    if (!channel) return;
+    channel.send(`Hombreeee, ${member}`);
+  });
 
 client.login(process.env.TOKEN)
