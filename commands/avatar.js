1eb24e9cac7c2de359690d4dd76d16ed5bbbf54e
Trissie <48584735+TrissieTheHusky@users.noreply.github.com>
2021-01-03 16:55:05 -0600

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,6 +2,11 @@ module.exports = {
 	name: 'avatar',
 	description: 'Sends the avatar image of the user tagged.',
 	execute(message) {
-        message.channel.send(message.mentions.users.first().avatarURL)
+        	const user = message.mentions.users.first() || message.author;
+    		const avatarEmbed = new Discord.RichEmbed()
+        	.setColor(0x333333)
+        	.setAuthor(user.username)
+        	.setImage(user.avatarURL);
+    	message.channel.send(avatarEmbed);
 	},
 };
