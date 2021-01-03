67ba5df0fc52d973f4f2b79df480ad66b3485247
Trissie <48584735+TrissieTheHusky@users.noreply.github.com>
2021-01-03 16:53:24 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message) {
-		message.channel.send('pong');
+		message.channel.send('🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms');
 	},
 };
